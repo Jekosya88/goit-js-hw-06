5c2cae84568e58ab43e289b00bc7442c44570ca5
Jekosya <zyv0007@gmail.com>
2025-07-20 10:20:17 +0200

    add jsik

--- js/task-3.js
+++ js/task-3.js
@@ -1,23 +1,23 @@
 class StringBuilder {
-    #value 
+    #value;
     constructor (initialValue) {
         this.#value = initialValue;
     }
-    get Value() {
+    getValue() {
         return this.#value;
     }
     padEnd(str) {
-        this.#value = this.#value + str;
+        this.#value +=  str;
     }
     padStart(str) {
-        this.#value = this.#value + str;
+        this.#value = str + this.#value ;
     }
     padBoth(str) {
-        this.#value = this.#value + str;
+        this.#value = str + this.#value + str;
     }
 } 
 const builder = new StringBuilder(".");
-console.log(builder.getValue()); // "."s
+console.log(builder.getValue()); // "."
 builder.padStart("^");
 console.log(builder.getValue()); // "^."
 builder.padEnd("^");
